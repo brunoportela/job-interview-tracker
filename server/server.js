200491d3191a620d6ea04309edd7e346dae8cbe0
brunoportela <13480425+brunoportela@users.noreply.github.com>
2021-05-22 23:10:35 -0700

    update port

--- server/server.js
+++ server/server.js
@@ -60,4 +60,6 @@ app.delete('/api/interviews/:id', (req, res) => {
 });
 
 // listen to port 3000
-app.listen(3000, () => console.log('Server Running'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
